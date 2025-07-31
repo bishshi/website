919f44cc99da3abae94b7840aa71c0eb51ddfaa2
bisnsh <bishsh2006@outlook.com>
2025-07-31 15:41:08 +0800

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-
+import expressiveCode from 'astro-expressive-code';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
@@ -7,5 +7,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [expressiveCode(),mdx(), sitemap()],
 });
