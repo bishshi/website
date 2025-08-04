c3d792dd205e0c905d855693c79d95f9f0ac65cd
bisnsh <bishsh2006@outlook.com>
2025-08-04 17:19:52 +0800

    Add statistical script

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import expressiveCode from 'astro-expressive-code';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import umami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [expressiveCode(),mdx(), sitemap()],
+	site: 'https://www.class612.bond',
+	integrations: [expressiveCode(),mdx(), sitemap(), 
+		umami(
+			{   hostUrl : "https://statistic.class612.bond/",
+				id: "44a27b01-3ea1-4a22-bb55-9a778dead980",
+			 }
+		)
+
+	],
 });
